5fb4fc38429fa8b3da1bc00696901a47dccb9d90
bndnt <67003594+bndnt@users.noreply.github.com>
2024-09-02 17:45:16 -0400

    index-forms-mob

--- src/js/index.js
+++ src/js/index.js
@@ -38,7 +38,50 @@ document.addEventListener('DOMContentLoaded', function () {
     loginBg.classList.remove('active');
   });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  // Получаем все элементы с классом .js-forms-popup
+  const popupBlocks = document.querySelectorAll('.js-forms-popup');
 
+  // Получаем элемент с классом .js-header__button-block
+  const openButton = document.querySelector('.js-header__button-block-mob');
+
+  // Получаем элемент с классом .js-login-bg
+  const loginBg = document.querySelector('.js-login-bg');
+
+  // Добавление активного класса ко всем .js-forms-popup и к .js-login-bg по клику на .js-header__button-block
+  openButton.addEventListener('click', function (event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    popupBlocks.forEach(function (popup) {
+      popup.classList.add('active');
+    });
+    document.querySelector('#mobile_menu_toggle').classList.toggle('active');
+    document.querySelector('.mobile__menu').classList.toggle('active');
+
+    document.querySelector('.top__hamburger').classList.toggle('active');
+    loginBg.classList.add('active');
+  });
+
+  // Обработка кликов на кнопки закрытия .js-login-btn-close
+  popupBlocks.forEach(function (popup) {
+    const closeButton = popup.querySelector('.js-login-btn-close');
+    closeButton.addEventListener('click', function () {
+      popup.classList.remove('active');
+
+      // Проверка, если все .js-forms-popup не имеют активного класса, удалить активный класс у .js-login-bg
+      if (![...popupBlocks].some(popup => popup.classList.contains('active'))) {
+        loginBg.classList.remove('active');
+      }
+    });
+  });
+
+  // Скрытие всех .js-forms-popup и .js-login-bg по клику на .js-login-bg
+  loginBg.addEventListener('click', function () {
+    popupBlocks.forEach(function (popup) {
+      popup.classList.remove('active');
+    });
+    loginBg.classList.remove('active');
+  });
+});
 document.addEventListener('DOMContentLoaded', function () {
   const showMoreButton = document.querySelector('.songs-show-more__button');
   const showMoreText = document.querySelector('.songs-show-more__span');
